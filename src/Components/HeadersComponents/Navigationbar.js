659feb757bf379ae15f8031e958779bad2a16d3b
Fynn Emmanuel Junior <fynn.emmanuel100@gmail.com>
2022-07-08 12:38:13 +0000

    Added a seach icon to the Side bar

--- src/Components/HeadersComponents/Navigationbar.js
+++ src/Components/HeadersComponents/Navigationbar.js
@@ -1,6 +1,6 @@
 function Navigationbar() {
     return (
-        <div className="navbar" style={{marginTop: '40px', borderBottom: '1px solid gray'}}>
+        <div className="navbar" style={{marginTop: '40px', borderBottom: '1px solid #ccc'}}>
            <ul className="navbarlist" style={{display: 'flex', listStyleType: 'none', justifyContent: 'space-evenly', alignContent: 'center', fontFamily: 'Helvetica', fontSize: '12px', width: '70%', margin: 'auto', marginBottom: '20px'}}>
              <li>SUITS</li>
              <li>SHIRTS</li>
--- src/Components/HeadersComponents/Side.js
+++ src/Components/HeadersComponents/Side.js
@@ -1,9 +1,10 @@
+import {FaSearch} from "react-icons/fa";
 function Side() {
     return (
         <div className="side">
              <ul className="sidebar" style={{fontFamily: 'Helvetica', fontSize: '15px'}}>
                 <li>Sign In / Register</li>
-                <li>Search</li>
+                <li><FaSearch style={{color: '#000', marginRight: '5px'}}/>Search</li>
                 <li style={{color: '#000'}}>Cart(0)</li>
             </ul>
         </div>
